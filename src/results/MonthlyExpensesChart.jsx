db20e128e291106047886ae3985df7cc98b3bdaa
kioannou <kostazj@gmail.com>
2022-04-03 23:17:32 +0300

    feat: add tooltips in pie chart

--- src/results/MonthlyExpensesChart.jsx
+++ src/results/MonthlyExpensesChart.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import {
   PieChart,
@@ -5,9 +6,52 @@ import {
   Cell,
   Label,
   ResponsiveContainer,
+  Tooltip,
 } from 'recharts';
 import PropTypes from 'prop-types';
 import { formatInDollars } from '../shared/utilities';
+
+function CustomTooltip({ payload }) {
+  if (payload.length === 0) {
+    return null;
+  }
+  const data = payload[0].payload;
+  return (
+    <div style={{
+      background: '#FAFCFF',
+      borderRadius: '3px',
+      padding: '10px',
+      boxShadow: `
+           0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+           0 6.7px 5.3px rgba(0, 0, 0, 0.048),
+           0 12.5px 10px rgba(0, 0, 0, 0.06),
+           0 22.3px 17.9px rgba(0, 0, 0, 0.072),
+           0 41.8px 33.4px rgba(0, 0, 0, 0.086),
+           0 100px 80px rgba(0, 0, 0, 0.12)
+          `,
+    }}
+    >
+      <div style={{
+        fontSize: '14px',
+        fontWeight: '500',
+        fontFamily: 'Graphik',
+        color: data.color,
+      }}
+      >
+        {`${data.text}: `}
+      </div>
+      <div style={{
+        fontSize: '14px',
+        fontFamily: 'Graphik',
+        fontWeight: '500',
+        color: data.color,
+      }}
+      >
+        {formatInDollars(data.value)}
+      </div>
+    </div>
+  );
+}
 
 // eslint-disable-next-line react/prop-types
 function CustomLabel({ viewBox, value1, value2 }) {
@@ -59,6 +103,7 @@ export default function MonthlyExpensesChart({ expenses, totalMonthlyExpenses })
             <Cell key={expense.text.replaceAll(' ', '_')} stroke={expense.color} fill={expense.color} />
           ))}
         </Pie>
+        <Tooltip content={<CustomTooltip />} />
       </PieChart>
     </ResponsiveContainer>
   );
